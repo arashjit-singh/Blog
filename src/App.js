de447e2aafb16af18544bc92e7fc30dff604a38b
Arashjit Singh <arashjit.singhh@gmail.com>
2023-02-14 09:13:36 +0000

    Added React-Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>My Blog</h1>
+      <div>Welcome to Blog!</div>
+    </div>
+  );
 }
 
 export default App;
